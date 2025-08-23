d0553f36fcc7649a274439f718ef6cf90f8fc795
Sumit Yadav <iamsumitydv@gmail.com>
2025-08-24 04:20:18 +0530

    feat: added values and variables with first commit

---
+++ Hands-on/1-values-and-variables/script.js
@@ -0,0 +1,25 @@
+/**
+ * JavaScript - JavaScript is dynamic computer programming language that is used for web development.
+ *              It is lightweight and most commonly used as a part of webpages whose implementation allows client side script interactions with user
+ *              and make dynamic pages. It is just-in-time compiled programming language with object oriented capabilities.
+ *
+ * Single Threaded Programming Language - that means it has a single call stack and can do one thing at a time.
+ *
+ * JavaScript is a - High Level, Prototype based object oriented, Multi Paradigm, just-in-time compiled, dynamic and single threaded, garbage collected programmin language with first class functions and non-blocking event loop concurrency model.
+ *
+ * Values and Variables - When we declare using let, const and var that means that is variable and when we are assign any value inside to variable that is value.
+ * const - initialize immediately after declaration. (block scoped)
+ * let - we can declare and initialize when we required. (block scoped)
+ * var - we can declare and initialize later. (either function scope or global scope)
+ */
+
+const country = "India";
+let continent = "Asia";
+let population = 1450;
+
+console.log("country = ", country);
+console.log("continent = ", continent);
+console.log("population = ", population);
+
+population = 1350;
+console.log("After changed in population = ", population);
